9d6eee5e38aa3002a1c87438a7f08bb0b911f041
Ismael-Oliveira <ismaeloliveira@inatel.br>
2021-11-14 20:33:15 -0300

    ajustando url do ambiente de produção.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURLBase: "https://gym-workout-tcc.herokuapp.com/",
+  apiURLBase: "https://gym-workout-tcc.herokuapp.com",
   clientId: "my-gym-workout-app",
   clientSecret: "@321",
   getTokenURL: "/oauth/token"
